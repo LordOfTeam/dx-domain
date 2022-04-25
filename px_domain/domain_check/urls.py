08a3f688861382bc59c835cf0e8f7f9c38e68516
0arshost <lpbdzmx@outlook.com>
2022-04-25 22:42:48 +0800

    fix

--- px_domain/domain_check/urls.py
+++ px_domain/domain_check/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from domain_check.api import views
 
 urlpatterns = [
-    path(r'check/', views.DomainCheckView.as_view()),
+    path(r'check', views.DomainCheckView.as_view()),
 ]   

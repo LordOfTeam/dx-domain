a4d66c0db61b5f6396f26b131c441e7cf7f27c18
0arshost <lpbdzmx@outlook.com>
2022-04-26 20:44:53 +0800

    fix

--- px_domain/domain_check/urls.py
+++ px_domain/domain_check/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from domain_check.api import views
 
 urlpatterns = [
-    path(r'check', views.DomainCheckView.as_view()),
+    # path('check', views.DomainCheckView.as_view()),
+    path('check/', views.DomainCheckView.as_view()),
 ]   
--- px_domain/px_domain/urls.py
+++ px_domain/px_domain/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.urls import path, include
 
 urlpatterns = [
-    url(r'^domain/', include("domain_check.urls")),
+    path('domain/', include("domain_check.urls")),
 ]
